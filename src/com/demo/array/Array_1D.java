f7514f5c5ebc87bf4da322b0aa6db9c4830ac570
DevanshiBakraniya21 <devanshibakraniya21.gmail.com>
2022-10-18 23:48:03 +0530

    Array Basic Functionality

---
+++ src/com/demo/array/Array_1D.java
@@ -0,0 +1,32 @@
+package com.demo.array;
+
+import java.util.Scanner;
+
+/**
+ * Find List of Even Number
+ * @author devanshi
+ *
+ */
+public class Array_1D {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Array Length: ");
+		int size = s.nextInt();
+		int[] a1 = new int[size];
+		
+		for(int j = 0; j < size; j++) {
+			System.out.println("Enter Array Value of "+j+":");
+			a1[j] = s.nextInt();
+		}
+		
+		System.out.println("Even number:");
+		for(int j = 0; j < size; j++) {
+			if(a1[j]%2 == 0) {
+				System.out.println("a1["+j+"] = "+ a1[j]+"\n");
+			}
+		}
+	}
+
+}
---
+++ src/com/demo/array/Array_2D.java
@@ -0,0 +1,38 @@
+package com.demo.array;
+
+import java.util.Scanner;
+
+/**
+ * Input no. of matrix and show in table format
+ * @author devanshi
+ *
+ */
+public class Array_2D {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter matrix ");
+		System.out.println("Row Size: ");
+		int row = s1.nextInt();
+		System.out.println("Column Size: ");
+		int column = s1.nextInt();
+		int[][] a1 = new int[row][column];
+		
+		for(int i=0; i < row; i++) {
+			for(int j=0; j<column; j++) {
+				System.out.println("Enter a["+i+"]["+j+"] :");
+				a1[i][j] = s1.nextInt();
+			}
+		}
+		
+		System.out.println("Output: ");
+		for(int i=0; i < row; i++) {
+			for(int j=0; j < column; j++) {
+				System.out.print(a1[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
